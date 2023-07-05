855ac333fd02a1d29d587317f4bc2892857ac70a
Ali <alidrl26@gmail.com>
2023-07-05 14:23:24 +0200

    es6 class work

--- Indecision App/src/App.js
+++ Indecision App/src/App.js
@@ -1,17 +1,19 @@
 import Challenge from "./Challenge";
-import BuilIt from "./Compenents/BuilIt";
+import BuilIt from "./Playgound/BuilIt";
+import Es6Classes1 from "./Playgound/Es6Classes1";
+
 // import Train from "./Train";
 // import Playgorund from "./Playgorund";
 
 function App() {
   return (
     <div>
+      {/* <Challenge /> */}
+
+      <Es6Classes1 />
       {/* <Train/> */}
-      <Challenge/>
-
-
       {/* <Playgorund/> */}
-      <BuilIt/>
+      {/* <BuilIt/> */}
     </div>
   );
 }

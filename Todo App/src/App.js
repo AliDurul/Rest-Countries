b5dc2c9cccc14c27b374b2917fbb245eb2c3eb3a
Ali <alidrl26@gmail.com>
2023-07-14 11:14:07 +0200

    projecte completed

--- Todo App/src/App.js
+++ Todo App/src/App.js
@@ -12,7 +12,7 @@ function App() {
   }, []);
 
   useEffect(() => {
-    if(tasks.length === 0) return;
+    if (tasks.length === 0) return;
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
@@ -20,19 +20,55 @@ function App() {
     setTasks([...tasks, { name: name, done: false }]);
   };
 
-const updateTaskDone = (taskIndex,newDone) => {
-  setTasks(prev => {
-    const newTasks = [...prev];
-    newTasks[taskIndex].done = newDone
-    return newTasks
+  const updateTaskDone = (taskIndex, newDone) => {
+    setTasks((prev) => {
+      const newTasks = [...prev];
+      newTasks[taskIndex].done = newDone;
+      return newTasks;
+    });
+  };
+
+  const numberComplete = tasks.filter((task) => task.done).length;
+  const numberTotal = tasks.length;
+
+  const getMessage = () => {
+    const percentage = (numberComplete / numberTotal) * 100;
+    if (percentage === 0) return "Try to do at least one! 🙏🏼";
+    if (percentage === 100) return "Nice Job for today! 👏🏼";
+
+    return "Keep it going 💪🏼";
+  };
+
+function removeTask(indexToRemove){
+setTasks(prev =>{
+  return prev.filter((index)=> index !== indexToRemove)
+})
+}
+
+const renameTask = (index,newName) => {
+  setTasks(prev =>{
+    const newTask = [...prev]
+    newTask[index].name = newName
+    return newTask;
   })
 }
 
+
   return (
     <main>
+      <h1>
+        {numberComplete}/{numberTotal} Complete
+      </h1>
+      <h2>{getMessage()}</h2>
       <TaskForm onAdd={addTask} />
       {tasks.map((task, index) => (
-        <Tasks key={index} {...task} onToggle = {(done) =>updateTaskDone(index,done) } />
+        <Tasks
+          key={index}
+          {...task}
+          onRename={newName =>renameTask(index,newName)}
+          onTrash={()=>removeTask(index)}
+          onToggle={(done) => updateTaskDone(index, done)}
+        />
       ))}
     </main>
   );

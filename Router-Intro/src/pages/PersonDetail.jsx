eadcf264e8250ea8a4e963db74e1aecb73739793
Ali <alidrl26@gmail.com>
2023-07-20 22:04:17 +0200

    routes files added

--- Router-Intro/src/pages/PersonDetail.jsx
+++ Router-Intro/src/pages/PersonDetail.jsx
@@ -1,23 +1,42 @@
 import React, { useEffect, useState } from 'react'
-import {useNavigate, useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import NotFound from "./NotFound";
+import spinner from "../img/Spinner-2.gif"
 
 const PersonDetail = () => {
     let { id } = useParams()
     const [person, setPerson] = useState()
+    const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(true)
+
 
     const getPerson = () => {
         fetch(`https://reqres.in/api/users/${id}`)
             .then((res) => res.json())
             .then((data) => setPerson(data.data))
-            .catch((err) => console.log(err));
+            .catch((err) => {
+                setError(true);
+                console.log(err);
+            })
+            .finally(() => setLoading(false));
     };
 
     useEffect(() => {
         getPerson()
     }, [])
-
     let navigate = useNavigate()
+
+    if (error) {
+        return <NotFound />;
+    } else if (loading) {
+        return (
+            <div className="text-center mt-4">
+                <img src={spinner} alt="spinner" />
+            </div>
+        );
+    }
     return (
+
         <div className="container text-center">
             <h3>
                 {person?.first_name} {person?.last_name}

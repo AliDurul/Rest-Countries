1bdb85da5034c2b45220f24871a1eaf12939daf0
Ali <alidrl26@gmail.com>
2023-07-20 14:36:05 +0200

    style.css updated

--- Recipe App/src/pages/Recipe.jsx
+++ Recipe App/src/pages/Recipe.jsx
@@ -9,8 +9,10 @@ const Recipe = () => {
     console.log(Mealid);
 
     const getDatabyId = async () => {
-        const data = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${Mealid}`)
-        setMeal(data.data.meals[0])
+        if (Mealid) {
+            const { data } = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${Mealid}`)
+            setMeal(data.meals[0])
+        }
     }
     console.log(meal);
     useEffect(() => {
@@ -21,7 +23,79 @@ const Recipe = () => {
 
     return (
         <>
-            <h1>hadi bakalim</h1>
+            {
+                meal &&
+
+                <div className="content">
+                    <img src={meal.strMealThumb} alt="" />
+                    <div className="inner-content">
+                        <h1>{meal.strMeal}</h1>
+                        <h2>{meal.strArea} Food</h2>
+                        <h3>{meal.strCategory}</h3>
+                    </div>
+
+                    <div className="recipe-details">
+                        <div className="ingredients">
+                            <h2>Ingredients</h2>
+                            <table >
+                                <tbody>
+                                    <tr>
+                                        <td><h4>{(meal.strIngredient1).toUpperCase()} </h4> </td>
+                                        <td><> : {meal.strMeasure1}</></td>
+                                    </tr>
+                                    <tr>
+                                        <td><h4>{(meal.strIngredient2).toUpperCase()} </h4> </td>
+                                        <td><> : {meal.strMeasure2}</></td>
+                                    </tr>
+                                    <tr>
+                                        <td><h4>{(meal.strIngredient3).toUpperCase()} </h4> </td>
+                                        <td><> : {meal.strMeasure3}</></td>
+                                    </tr>
+                                    <tr>
+                                        <td><h4>{(meal.strIngredient4).toUpperCase()} </h4> </td>
+                                        <td><> : {meal.strMeasure4}</></td>
+                                    </tr>
+                                    <tr>
+                                        <td><h4>{(meal.strIngredient5).toUpperCase()} </h4> </td>
+                                        <td><> : {meal.strMeasure5}</></td>
+                                    </tr>
+                                    <tr>
+                                        <td><h4>{(meal.strIngredient6).toUpperCase()} </h4> </td>
+                                        <td><> : {meal.strMeasure6}</></td>
+                                    </tr>
+                                    <tr>
+                                        <td><h4>{(meal.strIngredient7).toUpperCase()}  </h4> </td>
+                                        <td><> : {meal.strMeasure7}</></td>
+                                    </tr>
+                                    <tr>
+                                        <td><h4>{(meal.strIngredient8).toUpperCase()} </h4> </td>
+                                        <td><> : {meal.strMeasure8}</></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="instructions">
+                            <h2>Instructions</h2>
+                            <h4>{meal.strInstructions}</h4>
+                        </div>
+                    </div>
+                    <div className="video">
+
+                        {/* setVurl(meal.strYoutube)
+                                //const str=meal.strYoutube.split("=");
+                                //state=str[str.length-1];
+                                //state="hj"    */}
+
+
+                        <iframe width="
+                        100%" height="515" title="recipeVideo"
+                            src={`https://www.youtube.com/embed/${Mealid}`}>
+                        </iframe>
+                    </div>
+
+                </div>
+
+            }
         </>
     )
 }
